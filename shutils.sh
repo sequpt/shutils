68a3254c03f755af61b85fc111aa94437470c1c1
sequpt <sequpt@protonmail.com>
2022-11-27 14:46:48 +0000

    Add `_has_cmd()`

--- shutils.sh
+++ shutils.sh
@@ -51,16 +51,66 @@ ERR_EXEC=126
 # Command not found
 ERR_NOCMD=127
 ################################################################################
+# INTERNAL UTILITIES
+################################################################################
+#-------------------------------------------------------------------------------
+# _has_cmd()
+#-------------------------------------------------------------------------------
+# Check if a given command exists and return true if it does and false
+# otherwise.
+#
+# The command name and each of its arguments(if they exist) must be given as
+# separate arguments to `_has_cmd()`. Example for `ls -l -a`:
+# RIGHT: - `_has_cmd "ls" "-l" "-a"`
+#        - `_has_cmd ls -l -a`
+# WRONG: - `_has_cmd "ls -l -a"`
+#        - `_has_cmd "ls -l" "-a"`
+#        - `_has_cmd "ls" "-l -a"`
+#
+# If arguments are passed to the command, the existence of the command AND the
+# arguments are tested. In this case, `_has_cmd()` will return false if the
+# command exists but its arguments are invalid.
+#
+# @args
+# $1 [REQ]: Command name
+# $2+[OPT]: Arguments to the command
+#
+# @return
+# - @success: true
+# - @failure: false
+#
+# @warning
+# - Exit with `$ERR_USAGE` if no argument is given.
+#
+# @example
+# _has_cmd="$(_has_cmd "ls")"
+# _has_cmd="$(_has_cmd "tput" "sgr")"
+# _has_cmd "grep" && printf "Has grep: true\n"
+#-------------------------------------------------------------------------------
+_has_cmd() {
+    _arg_cnt="$#"
+    # Exit with `$ERR_USAGE` if no argument is given.
+    if [ "$_arg_cnt" -eq 0 ]; then
+        printf "Error: _has_cmd() needs at least 1 argument!\n" 1>&2
+        exit "$ERR_USAGE"
+    fi
+    _cmd="$1"
+    # Command is passed without arguments
+    if [ "$_arg_cnt" -eq 1 ]; then
+        command -v "$_cmd" > "/dev/null"
+    # Command is passed with one ore more arguments
+    else
+        shift 1
+        _args="$*"
+        command "$_cmd" "$_args" > "/dev/null" 2>&1
+    fi
+}
+################################################################################
 # TERMINAL
 # see:
 # - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
 # - https://en.wikipedia.org/wiki/ANSI_escape_code
 ################################################################################
-# Check if `tput` is available
-_has_tput=false
-if command -v tput > "/dev/null"; then
-    _has_tput=true
-fi
 # ASCII code for escape character
 # Octal: 033 | Decimal: 27 | Hexadecimal: 0x1B(_esc="\x1b")
 # See:
@@ -77,58 +127,55 @@ _csi="${_esc}["
 # - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Pm-m.1CA7
 # - https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
 # Clear all formatting - CSI 0 m
-t_clr=; "$_has_tput" && tput sgr0 > "/dev/null" && t_clr="$(tput sgr0)"
+t_clr=; _has_cmd "tput" "sgr0" && t_clr="$(tput sgr0)"
 # Bold ON - CSI 1 m
-fg_bold=; "$_has_tput" && tput bold > "/dev/null" && fg_bold="$(tput bold)"
+fg_bold=; _has_cmd tput bold && fg_bold="$(tput bold)"
 # Bold OFF - CSI 22 m(same as dim off)
 fg_clr_bold="${_csi}22m"
 # Dim ON - CSI 2 m
-fg_dim=; "$_has_tput" && tput dim > "/dev/null" && fg_dim="$(tput dim)"
+fg_dim=; _has_cmd tput dim && fg_dim="$(tput dim)"
 # Dim OFF - CSI 22 m(same as bold off)
 fg_clr_dim="${_csi}22m"
 # Italic ON - CSI 3 m
-fg_it=; "$_has_tput" && tput sitm > "/dev/null" && fg_it="$(tput sitm)"
+fg_it=; _has_cmd tput sitm && fg_it="$(tput sitm)"
 # Italic OFF - CSI 23 m
-fg_clr_it=; "$_has_tput" && tput ritm > "/dev/null" && fg_clr_it="$(tput ritm)"
+fg_clr_it=; _has_cmd tput ritm && fg_clr_it="$(tput ritm)"
 # Underline ON - CSI 4 m
-fg_ul=; "$_has_tput" && tput smul > "/dev/null" && fg_ul="$(tput smul)"
+fg_ul=; _has_cmd tput smul && fg_ul="$(tput smul)"
 # Underline OFF - CSI 24 m
-fg_clr_ul=; "$_has_tput" && tput rmul > "/dev/null" && fg_clr_ul="$(tput rmul)"
+fg_clr_ul=; _has_cmd tput rmul && fg_clr_ul="$(tput rmul)"
 # Blink - CSI 5 m
-fg_blink=; "$_has_tput" && tput blink > "/dev/null" && fg_blink="$(tput blink)"
+fg_blink=; _has_cmd tput blink && fg_blink="$(tput blink)"
 # Blink OFF - CSI 25 m
 fg_clr_blink="${_csi}25m"
 # Invert text/background colors - CSI 7 m
-t_rev=; "$_has_tput" && tput rev > "/dev/null" && t_rev="$(tput rev)"
+t_rev=; _has_cmd tput rev && t_rev="$(tput rev)"
 # Strike ON - CSI 9 m
 fg_strike="${_csi}9m"
 # Strike OFF - CSI 29 m
 fg_clr_strike="${_csi}29m"
 
 # Check if `tput setaf` is available
-_has_tput_setaf=false
-if "$_has_tput" && tput setaf > "/dev/null"; then
-    _has_tput_setaf=true
-fi
+_has_tput_setaf=$(_has_cmd tput setaf)
 # Default foreground color - CSI 39 m
 fg_default="${_csi}39m"
 # Foreground normal colors - CSI 30-37 m
-fg_black=;    "$_has_tput_setaf" && fg_black="$(tput setaf 0)"
-fg_red=;      "$_has_tput_setaf" && fg_red="$(tput setaf 1)"
-fg_green=;    "$_has_tput_setaf" && fg_green="$(tput setaf 2)"
-fg_yellow=;   "$_has_tput_setaf" && fg_yellow="$(tput setaf 3)"
-fg_blue=;     "$_has_tput_setaf" && fg_blue="$(tput setaf 4)"
-fg_magenta=;  "$_has_tput_setaf" && fg_magenta="$(tput setaf 5)"
-fg_cyan=;     "$_has_tput_setaf" && fg_cyan="$(tput setaf 6)"
-fg_lgray=;    "$_has_tput_setaf" && fg_lgray="$(tput setaf 7)"
-fg_dgray=;    "$_has_tput_setaf" && fg_dgray="$(tput setaf 8)"
-fg_lred=;     "$_has_tput_setaf" && fg_lred="$(tput setaf 9)"
-fg_lgreen=;   "$_has_tput_setaf" && fg_lgreen="$(tput setaf 10)"
-fg_lyellow=;  "$_has_tput_setaf" && fg_lyellow="$(tput setaf 11)"
-fg_lblue=;    "$_has_tput_setaf" && fg_lblue="$(tput setaf 12)"
-fg_lmagenta=; "$_has_tput_setaf" && fg_lmagenta="$(tput setaf 13)"
-fg_lcyan=;    "$_has_tput_setaf" && fg_lcyan="$(tput setaf 14)"
-fg_white=;    "$_has_tput_setaf" && fg_white="$(tput setaf 15)"
+fg_black=;    $_has_tput_setaf && fg_black="$(tput setaf 0)"
+fg_red=;      $_has_tput_setaf && fg_red="$(tput setaf 1)"
+fg_green=;    $_has_tput_setaf && fg_green="$(tput setaf 2)"
+fg_yellow=;   $_has_tput_setaf && fg_yellow="$(tput setaf 3)"
+fg_blue=;     $_has_tput_setaf && fg_blue="$(tput setaf 4)"
+fg_magenta=;  $_has_tput_setaf && fg_magenta="$(tput setaf 5)"
+fg_cyan=;     $_has_tput_setaf && fg_cyan="$(tput setaf 6)"
+fg_lgray=;    $_has_tput_setaf && fg_lgray="$(tput setaf 7)"
+fg_dgray=;    $_has_tput_setaf && fg_dgray="$(tput setaf 8)"
+fg_lred=;     $_has_tput_setaf && fg_lred="$(tput setaf 9)"
+fg_lgreen=;   $_has_tput_setaf && fg_lgreen="$(tput setaf 10)"
+fg_lyellow=;  $_has_tput_setaf && fg_lyellow="$(tput setaf 11)"
+fg_lblue=;    $_has_tput_setaf && fg_lblue="$(tput setaf 12)"
+fg_lmagenta=; $_has_tput_setaf && fg_lmagenta="$(tput setaf 13)"
+fg_lcyan=;    $_has_tput_setaf && fg_lcyan="$(tput setaf 14)"
+fg_white=;    $_has_tput_setaf && fg_white="$(tput setaf 15)"
 # Foreground bold colors
 fg_b_black="$fg_bold$fg_black"
 fg_b_red="$fg_bold$fg_red"
@@ -165,26 +212,23 @@ fg_d_lcyan="$fg_dim$fg_lcyan"
 fg_d_white="$fg_dim$fg_white"
 
 # Check if `tput setab` is available
-_has_tput_setab=false
-if "$_has_tput" && tput setab > "/dev/null"; then
-    _has_tput_setab=true
-fi
+_has_tput_setab="$(_has_cmd tput setab)"
 # Default background color - CSI 49 m
 bg_default="${_csi}49m"
 # Background colors - CSI 40-47 m
-bg_black=;    "$_has_tput_setab" && bg_black="$(tput setab 0)"
-bg_red=;      "$_has_tput_setab" && bg_red="$(tput setab 1)"
-bg_green=;    "$_has_tput_setab" && bg_green="$(tput setab 2)"
-bg_yellow=;   "$_has_tput_setab" && bg_yellow="$(tput setab 3)"
-bg_blue=;     "$_has_tput_setab" && bg_blue="$(tput setab 4)"
-bg_magenta=;  "$_has_tput_setab" && bg_magenta="$(tput setab 5)"
-bg_cyan=;     "$_has_tput_setab" && bg_cyan="$(tput setab 6)"
-bg_lgray=;    "$_has_tput_setab" && bg_lgray="$(tput setab 7)"
-bg_dgray=;    "$_has_tput_setab" && bg_dgray="$(tput setab 8)"
-bg_lred=;     "$_has_tput_setab" && bg_lred="$(tput setab 9)"
-bg_lgreen=;   "$_has_tput_setab" && bg_lgreen="$(tput setab 10)"
-bg_lyellow=;  "$_has_tput_setab" && bg_lyellow="$(tput setab 11)"
-bg_lblue=;    "$_has_tput_setab" && bg_lblue="$(tput setab 12)"
-bg_lmagenta=; "$_has_tput_setab" && bg_lmagenta="$(tput setab 13)"
-bg_lcyan=;    "$_has_tput_setab" && bg_lcyan="$(tput setab 14)"
-bg_white=;    "$_has_tput_setab" && bg_white="$(tput setab 15)"
+bg_black=;    $_has_tput_setab && bg_black="$(tput setab 0)"
+bg_red=;      $_has_tput_setab && bg_red="$(tput setab 1)"
+bg_green=;    $_has_tput_setab && bg_green="$(tput setab 2)"
+bg_yellow=;   $_has_tput_setab && bg_yellow="$(tput setab 3)"
+bg_blue=;     $_has_tput_setab && bg_blue="$(tput setab 4)"
+bg_magenta=;  $_has_tput_setab && bg_magenta="$(tput setab 5)"
+bg_cyan=;     $_has_tput_setab && bg_cyan="$(tput setab 6)"
+bg_lgray=;    $_has_tput_setab && bg_lgray="$(tput setab 7)"
+bg_dgray=;    $_has_tput_setab && bg_dgray="$(tput setab 8)"
+bg_lred=;     $_has_tput_setab && bg_lred="$(tput setab 9)"
+bg_lgreen=;   $_has_tput_setab && bg_lgreen="$(tput setab 10)"
+bg_lyellow=;  $_has_tput_setab && bg_lyellow="$(tput setab 11)"
+bg_lblue=;    $_has_tput_setab && bg_lblue="$(tput setab 12)"
+bg_lmagenta=; $_has_tput_setab && bg_lmagenta="$(tput setab 13)"
+bg_lcyan=;    $_has_tput_setab && bg_lcyan="$(tput setab 14)"
+bg_white=;    $_has_tput_setab && bg_white="$(tput setab 15)"
