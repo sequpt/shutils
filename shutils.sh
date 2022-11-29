5993c31c49933c264dbe4e88f690aad466f911e4
sequpt <sequpt@protonmail.com>
2022-11-29 06:04:47 +0000

    Add boolean flags testing the availability of some common commands

--- shutils.sh
+++ shutils.sh
@@ -51,6 +51,32 @@ ERR_EXEC=126
 # Command not found
 ERR_NOCMD=127
 ################################################################################
+# AVAILABILITY FLAG
+#
+# Boolean flags testing the availability of some common commands
+################################################################################
+has_apt_get=false;  command -v apt-get  > "/dev/null" && has_apt_get=true
+has_curl=false;     command -v curl     > "/dev/null" && has_curl=true
+has_cut=false;      command -v cut      > "/dev/null" && has_cut=true
+has_date=false;     command -v date     > "/dev/null" && has_date=true
+has_dirname=false;  command -v dirname  > "/dev/null" && has_dirname=true
+has_dpkg=false;     command -v dpkg     > "/dev/null" && has_dpkg=true
+has_git=false;      command -v git      > "/dev/null" && has_git=true
+has_grep=false;     command -v grep     > "/dev/null" && has_grep=true
+has_gzip=false;     command -v gzip     > "/dev/null" && has_gzip=true
+has_head=false;     command -v head     > "/dev/null" && has_head=true
+has_mkdir=false;    command -v mkdir    > "/dev/null" && has_mkdir=true
+has_realpath=false; command -v realpath > "/dev/null" && has_realpath=true
+has_rm=false;       command -v rm       > "/dev/null" && has_rm=true
+has_rsync=false;    command -v rsync    > "/dev/null" && has_rsync=true
+has_sed=false;      command -v sed      > "/dev/null" && has_sed=true
+has_shasum=false;   command -v shasum   > "/dev/null" && has_shasum=true
+has_su=false;       command -v su       > "/dev/null" && has_su=true
+has_sudo=false;     command -v sudo     > "/dev/null" && has_sudo=true
+has_tar=false;      command -v tar      > "/dev/null" && has_tar=true
+has_wget=false;     command -v wget     > "/dev/null" && has_wget=true
+has_xz=false;       command -v xz       > "/dev/null" && has_xz=true
+################################################################################
 # INTERNAL UTILITIES
 ################################################################################
 #-------------------------------------------------------------------------------
