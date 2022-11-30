430438e221f18245ab2c2c26f74eeddb6f019633
sequpt <sequpt@protonmail.com>
2022-11-30 11:32:51 +0000

    Refactor the terminal part
    - Add `has_tput_*` flags to check for `tput` capabilities
    - Add `t_invis`/`t_rm_invis and `t_ul2`/`t_rm_ul2`
    - Remove use of `command`
    - Change `fg_` prefix to `t_`
    - Change `fg_clr_` prefix to `t_rm_`
    - Change `bg_` prefix to `t_bg_`

--- shutils.sh
+++ shutils.sh
@@ -25,7 +25,7 @@
 # Shutils is a POSIX sh script providing some utilities functions and variables
 # for the following things:
 # - Exit codes
-# - Terminal text/background color and formating
+# - Terminal color and formating
 #
 # Functions and variables starting with an underscore(e.g., _var) are for
 # internal use only and shouldn't be used outside of this file.
@@ -78,6 +78,150 @@ has_tput=false;     command -v tput     > "/dev/null" && has_tput=true
 has_wget=false;     command -v wget     > "/dev/null" && has_wget=true
 has_xz=false;       command -v xz       > "/dev/null" && has_xz=true
 ################################################################################
+# TERMINAL
+#
+# Commands starting with `${_csi}` need to be used with the `%b` format
+# specifier when passed as arguments to `printf()` to be interpreted correctly
+# as escape sequences.
+# Example:
+# printf "%b\n" "${t_strike}Text${t_clr}" # Print a struck "Text" string
+# printf "%s\n" "${t_strike}Text${t_clr}" # Print "\033[9mText"
+#
+#
+# see:
+# - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+# - https://en.wikipedia.org/wiki/ANSI_escape_code
+################################################################################
+# ASCII code for escape character
+# Octal: 033 | Decimal: 27 | Hexadecimal: 0x1B(_esc="\x1b")
+# See:
+# - https://en.wikipedia.org/wiki/Escape_character#ASCII_escape_character
+_esc="\033"
+
+# CSI (Control Sequence Introducer)
+# See:
+# - https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
+_csi="${_esc}["
+#-------------------------------------------------------------------------------
+# SGR (Select Graphic Rendition)
+# See:
+# - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Pm-m.1CA7
+# - https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
+#-------------------------------------------------------------------------------
+has_tput_blink=false; tput blink > "/dev/null" 2>&1 && has_tput_blink=true
+has_tput_bold=false;  tput bold  > "/dev/null" 2>&1 && has_tput_bold=true
+has_tput_dim=false;   tput dim   > "/dev/null" 2>&1 && has_tput_dim=true
+has_tput_invis=false; tput invis > "/dev/null" 2>&1 && has_tput_invis=true
+has_tput_rev=false;   tput rev   > "/dev/null" 2>&1 && has_tput_rev=true
+has_tput_ritm=false;  tput ritm  > "/dev/null" 2>&1 && has_tput_ritm=true
+has_tput_rmul=false;  tput rmul  > "/dev/null" 2>&1 && has_tput_rmul=true
+has_tput_rmxx=false;  tput rmxx  > "/dev/null" 2>&1 && has_tput_rmxx=true
+has_tput_setab=false; tput setab > "/dev/null" 2>&1 && has_tput_setab=true
+has_tput_setaf=false; tput setaf > "/dev/null" 2>&1 && has_tput_setaf=true
+has_tput_sgr0=false;  tput sgr0  > "/dev/null" 2>&1 && has_tput_sgr0=true
+has_tput_sitm=false;  tput sitm  > "/dev/null" 2>&1 && has_tput_sitm=true
+has_tput_smul=false;  tput smul  > "/dev/null" 2>&1 && has_tput_smul=true
+has_tput_smxx=false;  tput smxx  > "/dev/null" 2>&1 && has_tput_smxx=true
+
+# Set mode
+t_bold=;   $has_tput_bold  && t_bold="$(tput bold)"      # Bold         - CSI 1 m
+t_dim=;    $has_tput_dim   && t_dim="$(tput dim)"        # Dim          - CSI 2 m
+t_it=;     $has_tput_sitm  && t_it="$(tput sitm)"        # Italic       - CSI 3 m
+t_ul=;     $has_tput_smul  && t_ul="$(tput smul)"        # Underline    - CSI 4 m
+t_blink=;  $has_tput_blink && t_blink="$(tput blink)"    # Blink        - CSI 5 m
+t_rev=;    $has_tput_rev   && t_rev="$(tput rev)"        # Reverse      - CSI 7 m
+t_invis=;  $has_tput_invis && t_invis="$(tput invis)"    # Invisible    - CIS 8 m
+t_strike=; $has_tput_smxx  && t_strike="$(tput smxx)"    # Strike       - CSI 9 m
+t_ul2="${_csi}21m"                                       # Underline x2 - CSI 21 m
+# Remove mode
+t_clr=;       $has_tput_sgr0  && t_clr="$(tput sgr0)"       # Reset all    - CSI 0 m
+t_rm_bold=;   $has_tput_bold  && t_rm_bold="${_csi}22m"     # Bold         - CSI 22 m
+t_rm_dim=;    $has_tput_dim   && t_rm_dim="${_csi}22m"      # Dim          - CSI 22 m
+t_rm_it=;     $has_tput_ritm  && t_rm_it="$(tput ritm)"     # Italic       - CSI 23 m
+t_rm_ul=;     $has_tput_rmul  && t_rm_ul="$(tput rmul)"     # Underline    - CSI 24 m
+t_rm_ul2=;    $has_tput_rmul  && t_rm_ul2="${t_rm_ul}"      # Underline x2 - CSI 24 m
+t_rm_blink=;  $has_tput_blink && t_rm_blink="${_csi}25m"    # Blink        - CSI 25 m
+t_rm_invis=;  $has_tput_invis && t_rm_invis="${_csi}28m"    # Invisible    - CSI 28 m
+t_rm_strike=; $has_tput_rmxx  && t_rm_strike="$(tput rmxx)" # Strike       - CSI 29 m
+#-------------------------------------------------------------------------------
+# Foreground colors
+#-------------------------------------------------------------------------------
+# Default - CSI 39 m
+t_default="${_csi}39m"
+# Normal
+t_black=;    $has_tput_setaf && t_black="$(tput setaf 0)"     # Black         - CSI 30 m
+t_red=;      $has_tput_setaf && t_red="$(tput setaf 1)"       # Red           - CSI 31 m
+t_green=;    $has_tput_setaf && t_green="$(tput setaf 2)"     # Green         - CSI 32 m
+t_yellow=;   $has_tput_setaf && t_yellow="$(tput setaf 3)"    # Yellow        - CSI 33 m
+t_blue=;     $has_tput_setaf && t_blue="$(tput setaf 4)"      # Blue          - CSI 34 m
+t_magenta=;  $has_tput_setaf && t_magenta="$(tput setaf 5)"   # Magenta       - CSI 35 m
+t_cyan=;     $has_tput_setaf && t_cyan="$(tput setaf 6)"      # Cyan          - CSI 36 m
+t_lgray=;    $has_tput_setaf && t_lgray="$(tput setaf 7)"     # Light Gray    - CSI 37 m
+t_dgray=;    $has_tput_setaf && t_dgray="$(tput setaf 8)"     # Dark Gray     - CSI 90 m
+t_lred=;     $has_tput_setaf && t_lred="$(tput setaf 9)"      # Light Red     - CSI 91 m
+t_lgreen=;   $has_tput_setaf && t_lgreen="$(tput setaf 10)"   # Light Green   - CSI 92 m
+t_lyellow=;  $has_tput_setaf && t_lyellow="$(tput setaf 11)"  # Light Yellow  - CSI 93 m
+t_lblue=;    $has_tput_setaf && t_lblue="$(tput setaf 12)"    # Light Yellow  - CSI 94 m
+t_lmagenta=; $has_tput_setaf && t_lmagenta="$(tput setaf 13)" # Light Magenta - CSI 95 m
+t_lcyan=;    $has_tput_setaf && t_lcyan="$(tput setaf 14)"    # Light Cyan    - CSI 96 m
+t_white=;    $has_tput_setaf && t_white="$(tput setaf 15)"    # White         - CSI 97 m
+# Bold
+t_b_black="$t_bold$t_black"
+t_b_red="$t_bold$t_red"
+t_b_green="$t_bold$t_green"
+t_b_yellow="$t_bold$t_yellow"
+t_b_blue="$t_bold$t_blue"
+t_b_magenta="$t_bold$t_magenta"
+t_b_cyan="$t_bold$t_cyan"
+t_b_lgray="$t_bold$t_lgray"
+t_b_dgray="$t_bold$t_dgray"
+t_b_lred="$t_bold$t_lred"
+t_b_lgreen="$t_bold$t_lgreen"
+t_b_lyellow="$t_bold$t_lyellow"
+t_b_lblue="$t_bold$t_lblue"
+t_b_lmagenta="$t_bold$t_lmagenta"
+t_b_lcyan="$t_bold$t_lcyan"
+t_b_white="$t_bold$t_white"
+# Dim
+t_d_black="$t_dim$t_black"
+t_d_red="$t_dim$t_red"
+t_d_green="$t_dim$t_green"
+t_d_yellow="$t_dim$t_yellow"
+t_d_blue="$t_dim$t_blue"
+t_d_magenta="$t_dim$t_magenta"
+t_d_cyan="$t_dim$t_cyan"
+t_d_lgray="$t_dim$t_lgray"
+t_d_dgray="$t_dim$t_dgray"
+t_d_lred="$t_dim$t_lred"
+t_d_lgreen="$t_dim$t_lgreen"
+t_d_lyellow="$t_dim$t_lyellow"
+t_d_lblue="$t_dim$t_lblue"
+t_d_lmagenta="$t_dim$t_lmagenta"
+t_d_lcyan="$t_dim$t_lcyan"
+t_d_white="$t_dim$t_white"
+#-------------------------------------------------------------------------------
+# Background colors
+#-------------------------------------------------------------------------------
+# Default - CSI 49 m
+t_bg_default="${_csi}49m"
+# Normal
+t_bg_black=;    $has_tput_setab && t_bg_black="$(tput setab 0)"     # Black         - CSI 40 m
+t_bg_red=;      $has_tput_setab && t_bg_red="$(tput setab 1)"       # Red           - CSI 41 m
+t_bg_green=;    $has_tput_setab && t_bg_green="$(tput setab 2)"     # Green         - CSI 42 m
+t_bg_yellow=;   $has_tput_setab && t_bg_yellow="$(tput setab 3)"    # Yellow        - CSI 43 m
+t_bg_blue=;     $has_tput_setab && t_bg_blue="$(tput setab 4)"      # Blue          - CSI 44 m
+t_bg_magenta=;  $has_tput_setab && t_bg_magenta="$(tput setab 5)"   # Magenta       - CSI 45 m
+t_bg_cyan=;     $has_tput_setab && t_bg_cyan="$(tput setab 6)"      # Cyan          - CSI 46 m
+t_bg_lgray=;    $has_tput_setab && t_bg_lgray="$(tput setab 7)"     # Light Gray    - CSI 47 m
+t_bg_dgray=;    $has_tput_setab && t_bg_dgray="$(tput setab 8)"     # Dark Gray     - CSI 100 m
+t_bg_lred=;     $has_tput_setab && t_bg_lred="$(tput setab 9)"      # Light Red     - CSI 101 m
+t_bg_lgreen=;   $has_tput_setab && t_bg_lgreen="$(tput setab 10)"   # Light Green   - CSI 102 m
+t_bg_lyellow=;  $has_tput_setab && t_bg_lyellow="$(tput setab 11)"  # Light Yellow  - CSI 103 m
+t_bg_lblue=;    $has_tput_setab && t_bg_lblue="$(tput setab 12)"    # Light Yellow  - CSI 104 m
+t_bg_lmagenta=; $has_tput_setab && t_bg_lmagenta="$(tput setab 13)" # Light Magenta - CSI 105 m
+t_bg_lcyan=;    $has_tput_setab && t_bg_lcyan="$(tput setab 14)"    # Light Cyan    - CSI 106 m
+t_bg_white=;    $has_tput_setab && t_bg_white="$(tput setab 15)"    # White         - CSI 107 m
+################################################################################
 # INTERNAL UTILITIES
 ################################################################################
 #-------------------------------------------------------------------------------
@@ -118,130 +262,3 @@ _assert_str_not_empty() {
         exit "$ERR_FAILURE"
     fi
 }
-################################################################################
-# TERMINAL
-# see:
-# - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
-# - https://en.wikipedia.org/wiki/ANSI_escape_code
-################################################################################
-# ASCII code for escape character
-# Octal: 033 | Decimal: 27 | Hexadecimal: 0x1B(_esc="\x1b")
-# See:
-# - https://en.wikipedia.org/wiki/Escape_character#ASCII_escape_character
-_esc="\033"
-
-# CSI (Control Sequence Introducer)
-# See:
-# - https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
-_csi="${_esc}["
-
-# SGR (Select Graphic Rendition)
-# See:
-# - https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Pm-m.1CA7
-# - https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
-# Clear all formatting - CSI 0 m
-t_clr=; command tput sgr0 > "/dev/null" 2>&1 && t_clr="$(tput sgr0)"
-# Bold ON - CSI 1 m
-fg_bold=; command tput bold > "/dev/null" 2>&1 && fg_bold="$(tput bold)"
-# Bold OFF - CSI 22 m(same as dim off)
-fg_clr_bold="${_csi}22m"
-# Dim ON - CSI 2 m
-fg_dim=; command tput dim > "/dev/null" 2>&1 && fg_dim="$(tput dim)"
-# Dim OFF - CSI 22 m(same as bold off)
-fg_clr_dim="${_csi}22m"
-# Italic ON - CSI 3 m
-fg_it=; command tput sitm > "/dev/null" 2>&1 && fg_it="$(tput sitm)"
-# Italic OFF - CSI 23 m
-fg_clr_it=; command tput ritm > "/dev/null" 2>&1 && fg_clr_it="$(tput ritm)"
-# Underline ON - CSI 4 m
-fg_ul=; command tput smul > "/dev/null" 2>&1 && fg_ul="$(tput smul)"
-# Underline OFF - CSI 24 m
-fg_clr_ul=; command tput rmul > "/dev/null" 2>&1 && fg_clr_ul="$(tput rmul)"
-# Blink - CSI 5 m
-fg_blink=; command tput blink > "/dev/null" 2>&1 && fg_blink="$(tput blink)"
-# Blink OFF - CSI 25 m
-fg_clr_blink="${_csi}25m"
-# Invert text/background colors - CSI 7 m
-t_rev=; command tput rev > "/dev/null" 2>&1 && t_rev="$(tput rev)"
-# Strike ON - CSI 9 m
-fg_strike="${_csi}9m"
-# Strike OFF - CSI 29 m
-fg_clr_strike="${_csi}29m"
-
-# Check if `tput setaf` is available
-_has_tput_setaf=; command tput setaf > "/dev/null" 2>&1 && _has_tput_setaf=true
-# Default foreground color - CSI 39 m
-fg_default="${_csi}39m"
-# Foreground normal colors - CSI 30-37 m
-fg_black=;    $_has_tput_setaf && fg_black="$(tput setaf 0)"
-fg_red=;      $_has_tput_setaf && fg_red="$(tput setaf 1)"
-fg_green=;    $_has_tput_setaf && fg_green="$(tput setaf 2)"
-fg_yellow=;   $_has_tput_setaf && fg_yellow="$(tput setaf 3)"
-fg_blue=;     $_has_tput_setaf && fg_blue="$(tput setaf 4)"
-fg_magenta=;  $_has_tput_setaf && fg_magenta="$(tput setaf 5)"
-fg_cyan=;     $_has_tput_setaf && fg_cyan="$(tput setaf 6)"
-fg_lgray=;    $_has_tput_setaf && fg_lgray="$(tput setaf 7)"
-fg_dgray=;    $_has_tput_setaf && fg_dgray="$(tput setaf 8)"
-fg_lred=;     $_has_tput_setaf && fg_lred="$(tput setaf 9)"
-fg_lgreen=;   $_has_tput_setaf && fg_lgreen="$(tput setaf 10)"
-fg_lyellow=;  $_has_tput_setaf && fg_lyellow="$(tput setaf 11)"
-fg_lblue=;    $_has_tput_setaf && fg_lblue="$(tput setaf 12)"
-fg_lmagenta=; $_has_tput_setaf && fg_lmagenta="$(tput setaf 13)"
-fg_lcyan=;    $_has_tput_setaf && fg_lcyan="$(tput setaf 14)"
-fg_white=;    $_has_tput_setaf && fg_white="$(tput setaf 15)"
-# Foreground bold colors
-fg_b_black="$fg_bold$fg_black"
-fg_b_red="$fg_bold$fg_red"
-fg_b_green="$fg_bold$fg_green"
-fg_b_yellow="$fg_bold$fg_yellow"
-fg_b_blue="$fg_bold$fg_blue"
-fg_b_magenta="$fg_bold$fg_magenta"
-fg_b_cyan="$fg_bold$fg_cyan"
-fg_b_lgray="$fg_bold$fg_lgray"
-fg_b_dgray="$fg_bold$fg_dgray"
-fg_b_lred="$fg_bold$fg_lred"
-fg_b_lgreen="$fg_bold$fg_lgreen"
-fg_b_lyellow="$fg_bold$fg_lyellow"
-fg_b_lblue="$fg_bold$fg_lblue"
-fg_b_lmagenta="$fg_bold$fg_lmagenta"
-fg_b_lcyan="$fg_bold$fg_lcyan"
-fg_b_white="$fg_bold$fg_white"
-# Foreground dim colors
-fg_d_black="$fg_dim$fg_black"
-fg_d_red="$fg_dim$fg_red"
-fg_d_green="$fg_dim$fg_green"
-fg_d_yellow="$fg_dim$fg_yellow"
-fg_d_blue="$fg_dim$fg_blue"
-fg_d_magenta="$fg_dim$fg_magenta"
-fg_d_cyan="$fg_dim$fg_cyan"
-fg_d_lgray="$fg_dim$fg_lgray"
-fg_d_dgray="$fg_dim$fg_dgray"
-fg_d_lred="$fg_dim$fg_lred"
-fg_d_lgreen="$fg_dim$fg_lgreen"
-fg_d_lyellow="$fg_dim$fg_lyellow"
-fg_d_lblue="$fg_dim$fg_lblue"
-fg_d_lmagenta="$fg_dim$fg_lmagenta"
-fg_d_lcyan="$fg_dim$fg_lcyan"
-fg_d_white="$fg_dim$fg_white"
-
-# Check if `tput setab` is available
-_has_tput_setab=; command tput setab > "/dev/null" 2>&1 && _has_tput_setab=true
-# Default background color - CSI 49 m
-bg_default="${_csi}49m"
-# Background colors - CSI 40-47 m
-bg_black=;    $_has_tput_setab && bg_black="$(tput setab 0)"
-bg_red=;      $_has_tput_setab && bg_red="$(tput setab 1)"
-bg_green=;    $_has_tput_setab && bg_green="$(tput setab 2)"
-bg_yellow=;   $_has_tput_setab && bg_yellow="$(tput setab 3)"
-bg_blue=;     $_has_tput_setab && bg_blue="$(tput setab 4)"
-bg_magenta=;  $_has_tput_setab && bg_magenta="$(tput setab 5)"
-bg_cyan=;     $_has_tput_setab && bg_cyan="$(tput setab 6)"
-bg_lgray=;    $_has_tput_setab && bg_lgray="$(tput setab 7)"
-bg_dgray=;    $_has_tput_setab && bg_dgray="$(tput setab 8)"
-bg_lred=;     $_has_tput_setab && bg_lred="$(tput setab 9)"
-bg_lgreen=;   $_has_tput_setab && bg_lgreen="$(tput setab 10)"
-bg_lyellow=;  $_has_tput_setab && bg_lyellow="$(tput setab 11)"
-bg_lblue=;    $_has_tput_setab && bg_lblue="$(tput setab 12)"
-bg_lmagenta=; $_has_tput_setab && bg_lmagenta="$(tput setab 13)"
-bg_lcyan=;    $_has_tput_setab && bg_lcyan="$(tput setab 14)"
-bg_white=;    $_has_tput_setab && bg_white="$(tput setab 15)"
